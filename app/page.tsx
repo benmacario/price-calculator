a6f8c4d47be5a177c4c77b00a322908b232b321b
Brendon Machado <benmacario.s@gmail.com>
2024-10-02 16:45:23 -0300

    fix: added suspense in page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import FormPriceCalculator from "@/components/server/form-price-calculator";
+import { Suspense } from "react";
 
 export default function Page() {
   return (
     <div className="my-6 mx-3">
-      <FormPriceCalculator />
+      <Suspense fallback={<div>Loading...</div>}>
+        <FormPriceCalculator />
+      </Suspense>
     </div>
   );
 }
